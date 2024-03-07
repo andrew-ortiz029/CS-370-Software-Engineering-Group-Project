56ab934799e7e3b88e40238c20132b078d23fb2a
Jason Gray <jgray7693@gmail.com>
2024-03-07 10:16:11 -0800

    Initial commit. Basic Spring Boot framework built.

---
+++ c4n_weather/src/test/java/com/c4n/c4n_weather/C4nWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c4n.c4n_weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C4nWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
