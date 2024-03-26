f4db96b469a2506c3be4bae1b223bdca4951d18a
Jason Gray <jgray7693@gmail.com>
2024-03-26 09:52:59 -0700

    Updated HelloController with base Mapping

--- c4n_weather/src/main/java/com/c4n/c4n_weather/Controllers/HelloController.java
+++ c4n_weather/src/main/java/com/c4n/c4n_weather/Controllers/HelloController.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.ResponseBody; 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
 
+
 @Controller
+@RequestMapping("/")
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("")
 	public String index() {
 		return "login";
 	}
